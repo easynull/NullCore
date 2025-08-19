fcf166d2e2d17ecd0327fd27a9893590f2600cf1
easynull <danror341@gmail.com>
2025-08-19 22:27:20 +0500

    Lockable system added

--- common/src/main/java/com/mw/nullcore/core/mixin/FontRenderMixin.java
+++ common/src/main/java/com/mw/nullcore/core/mixin/FontRenderMixin.java
@@ -10,15 +10,15 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-@Mixin(StringRenderOutput.class)
-public final class FontRenderMixin {
-    @Inject(method = "accept", at = @At("TAIL"))
-    private void nc$render(int index, Style style, int codePoint, CallbackInfoReturnable<Boolean> cir) {
-        if(style instanceof ShyStyle) {
-            StringRenderOutput output = (StringRenderOutput) ((Object) this);
-            float off = (float) Math.sin(Math.toDegrees((index * 0.02f + Utils.Render.getAnimationTick() * 0.005f * 2f)));
-            output.y += off;
-            output.color = Utils.Color.getCyclingColor(0.04f, 0xFFC8EBFF, 0xFFFFFFFF, 0xFFC00B7D);
-        }
-    }
-}
+//@Mixin(StringRenderOutput.class)
+//public final class FontRenderMixin {
+//    @Inject(method = "accept", at = @At("TAIL"))
+//    private void nc$render(int index, Style style, int codePoint, CallbackInfoReturnable<Boolean> cir) {
+//        if(style instanceof ShyStyle) {
+//            StringRenderOutput output = (StringRenderOutput) ((Object) this);
+//            float off = (float) Math.sin(Math.toDegrees((index * 0.02f + Utils.Render.getAnimationTick() * 0.005f * 2f)));
+//            output.y += off;
+//            output.color = Utils.Color.getCyclingColor(0.04f, 0xFFC8EBFF, 0xFFFFFFFF, 0xFFC00B7D);
+//        }
+//    }
+//}
